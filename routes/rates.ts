c72a8bb849d54f421f804e01cb1a7e7e290727c4
tiak7 <113570458+tiak7@users.noreply.github.com>
2022-10-07 15:37:27 -0400

    Update rates.ts

--- routes/rates.ts
+++ routes/rates.ts
@@ -3,10 +3,16 @@ const router = express.Router();
 
 import { getApiRequests } from "../model";
 
+/**
+ *returns a basic welcome response
+**/
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("welcome to the rates api!");
 });
 
+/**
+ *gets all api requests and returns json response
+**/
 router.get("/get", async (req: Request, res: Response, next: NextFunction) => {
   let rates = await getApiRequests();
   res.json(rates);
